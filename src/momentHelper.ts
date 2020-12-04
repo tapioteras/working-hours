ea89502d30d845f7e86df026ce4a49bfd3d1e995
Tapio Teras <tapio.teras@reaktor.com>
2020-12-04 13:24:44 +0200

    Create separate util for quarter time picker

---
+++ src/momentHelper.ts
@@ -0,0 +1,22 @@
+import moment from "moment"
+
+export const getNearestQuarterMoment = (currentMoment = moment()): moment.Moment => {
+  if([0,15,30,45].includes(currentMoment.minutes())) {
+    return currentMoment
+  }
+  const reaches = [[0, 15], [15, 30], [30, 45], [45, 60]]
+    .map(([a, b]) => ({ a, b, reachA: Math.abs(currentMoment.minutes() - a), reachB: Math.abs(currentMoment.minutes() - b) }))
+    .sort((a, b) => a.reachA > b.reachA || b.reachB > b.reachB ? 1 : -1)
+  console.log(reaches)
+  const { reachA, reachB, a , b } = reaches[0]
+
+  if (reachA < reachB) {
+    if (currentMoment.minutes() < a) {
+      return currentMoment.add(reachA, "minutes")
+    } else {
+      return currentMoment.subtract(reachA, "minutes")
+    }
+  } else {
+    return currentMoment.add(reachB, "minutes")
+  }
+}
