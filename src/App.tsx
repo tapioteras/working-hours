77251058104c330295019d65d518adc6b1be535a
Tapio Teras <tapio.teras@reaktor.com>
2020-12-10 16:16:07 +0200

    Refactor time tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,128 +1,53 @@
 import React, {useEffect, useState} from 'react';
-import moment from "moment-timezone";
-import { ChakraProvider, Input, Heading, Flex, Button, HStack, Box } from '@chakra-ui/react';
-import * as rawMoment from "moment";
+import { ChakraProvider, Input, Heading, Flex, Button, HStack, VStack, Box } from '@chakra-ui/react';
+import {getNearestQuarterMoment} from "./momentHelper";
+import moment from "moment";
+import useInterval from "@use-it/interval";
 
-interface TimePrintLayout {
-  hours: string
-  minutes: string
-  format: string
-  formatWithSeconds: string
-}
-
-interface NearestQuarterTime {
-  hours: any
-  round: number
-  reach: number
-  minutes: number
-  printLayout: string
-}
-
-const getTimePrintLayout = (): TimePrintLayout => {
-  const hours = "HH";
-  const minutes = "mm";
-  const seconds = "ss"
-  const format = `${hours}:${minutes}`;
-  const formatWithSeconds = `${hours}:${minutes}:${seconds}`
-  return { hours, minutes, format, formatWithSeconds };
-};
-
-interface QuarterWithReach {
-  round: number
-  reach: number
-}
-
-interface QuarterWithPrintLayout {
-  quarter: number
-  printLayout: string
-}
-
-const quarters: QuarterWithPrintLayout[] = [
-  {quarter: 0, printLayout: "00"},
-  {quarter: 15, printLayout: "15"},
-  {quarter: 30, printLayout: "30"},
-  {quarter: 45, printLayout: "45"},
-  {quarter: 60, printLayout: "00"}
-]
-const byReach = (minutes: number, round: number): QuarterWithReach => ({round, reach: Math.abs(minutes - round)})
-const byNearestReach = (a: QuarterWithReach, b: QuarterWithReach) => (a.reach > b.reach) ? 1 : -1
-
-const getNearestQuarterTimeByMinutes = (
-  returnObject = false,
-  hours = moment().format(getTimePrintLayout().hours),
-  minutes: number = moment().minutes(),
-): NearestQuarterTime | string => {
-  const quarterTimeObject = quarters
-    .map(({quarter, ...rest}) => ({...byReach(minutes, quarter), ...rest, hours, minutes}))
-    .sort(byNearestReach)
-    [0]
-  return returnObject ? quarterTimeObject : `${hours}:${quarterTimeObject.printLayout}`;
-}
-
-interface TimeTableCell {
-  id: number,
-  hour: number
-  minute: number
-}
-
-interface TimeTableRow {
-  id: number
-  actualStartTime: rawMoment.Moment,
-  startMoment : rawMoment.Moment,
-  startTime: TimeTableCell
-  endTime?: TimeTableCell
+interface Measurement {
+  start: moment.Moment
+  stop: moment.Moment
 }
 
 function App() {
-  const [currentTimeQuarter, setCurrentTimeQuarter] = useState(getNearestQuarterTimeByMinutes())
-  const [currentTimeMoment, setCurrentTimeMoment] = useState(moment())
-  const [timeTable, setTimeTable] = useState([])
-  const [currentTracker, setCurrentTracker] = useState(null)
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTimeQuarter(getNearestQuarterTimeByMinutes())
-      setCurrentTimeMoment(moment())
-    }, 500);
-    return () => clearInterval(interval);
-  }, []);
+  const [startMoment, setStartMoment] = useState()
+  const [currentMoment, setCurrentMoment] = useState(moment())
+  const [measurements, setMeasurements] = useState([])
+  useInterval(() => {
+      setCurrentMoment(moment())
+  }, 100);
+  const diff = (start: moment.Moment, stop: moment.Moment) => moment.utc(stop.diff(start))
+  const toMeasurementRow = (start: moment.Moment, stop: moment.Moment) => <>
+    {start.format("HH:mm")} - {stop.format("HH:mm")} = {
+    diff(start, stop).format("HH:mm")}
+    ({getNearestQuarterMoment(start).format("HH:mm")} - {getNearestQuarterMoment(stop).format("HH:mm")} = {getNearestQuarterMoment(diff(start, stop)).format("HH:mm")})
+  </>
+  const measurementsAsMilliseconds = () => [...measurements]
+    .map(({start, stop}) => diff(start, stop))
+    .map(duration => duration.milliseconds())
+  const onStart = () => {
+    setStartMoment(moment())
+  }
+  const onStop = () => {
+    setMeasurements([...measurements, { start: startMoment, stop: moment() } as Measurement])
+    setStartMoment(undefined)
+  }
+  const totalTime = moment.utc(measurementsAsMilliseconds().reduce((a,b) => a + b, 0)).format("HH:mm")
   return (
     <ChakraProvider>
-      <Heading padding={15} size="4xl">{currentTimeMoment.format(getTimePrintLayout().format)}</Heading>
-      <Flex width={200} paddingLeft={15} flexDirection="row">
-        <Input disabled value={currentTimeQuarter.toString()} marginRight={15} />
-        <Button onClick={() => {
-          const quarterTime: NearestQuarterTime = getNearestQuarterTimeByMinutes(true) as NearestQuarterTime
-          if (currentTracker) {
-            setTimeTable([
-              // @ts-ignore
-              ...timeTable, {
-                ...currentTracker,
-                endTime: {
-                  id: [...timeTable].length + 1,
-                  hours: quarterTime.hours,
-                  minutes: quarterTime.minutes
-                }
-              }
-            ])
-            setCurrentTracker(null)
-          } else {
-            // @ts-ignore
-            setCurrentTracker({
-              actualStartTime: moment().format(getTimePrintLayout().format)
-              startTime: currentTimeQuarter,
-              startMoment: currentTimeMoment,
-            } as TimeTableRow)
-          }
-        }}>{currentTracker ? "stop" : "start"}</Button>
-      </Flex>
-      {currentTracker && <Box padding={15}>{currentTracker.startTime}-{`${getNearestQuarterTimeByMinutes()} (${moment.utc(moment().diff(currentTracker.startMoment)).format(getTimePrintLayout().formatWithSeconds)})`}</Box>}
-      {timeTable.map(({ startTime, startMoment, endTime }, i) => (
-        <HStack key={`timetable-row-${i}`} paddingTop={15} paddingLeft={15} spacing="24px">
-          <Box>
-            {startTime} - {`${getNearestQuarterTimeByMinutes(false, endTime.hours, endTime.minutes)} (${endTime.hours}:${endTime.minutes})`}
-          </Box>
-        </HStack>
-      ))}
+      <Heading size="1xl">{currentMoment.format("HH:mm")}</Heading>
+      <Button disabled={!!startMoment} onClick={onStart}>start</Button>
+      {startMoment && <>
+          <Button onClick={onStop}>stop</Button>
+          <VStack>
+        {startMoment && <Box backgroundColor="green" spacing={10}>{toMeasurementRow(startMoment, moment())}</Box>}
+      </VStack>
+      </>}
+      <VStack>{measurements.reverse().map(({start, stop}, i) =>
+        <Box spacing={5} key={`measurement-row-${i}`}>
+          {toMeasurementRow(start, stop)}
+        </Box>)}</VStack>
+      <Box>total: {totalTime}</Box>
     </ChakraProvider>
   );
 }
