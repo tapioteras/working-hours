540bfd40d3f87696130498f51b6e0d86f73e62d0
Tapio Teras <tapio.teras@reaktor.com>
2020-12-22 11:42:35 +0200

    Add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { ChakraProvider, Input, Heading, Flex, Button, HStack, VStack, Box } from '@chakra-ui/react';
+import { ChakraProvider, Input, Heading, Flex, Button, HStack, Box } from '@chakra-ui/react';
 import {getNearestQuarterMoment} from "./momentHelper";
 import moment from "moment";
 import useInterval from "@use-it/interval";
@@ -7,31 +7,52 @@ import useInterval from "@use-it/interval";
 const format = "HH:mm:ss"
 const formatInput = "HH:mm"
 
-const WorkingPeriod = ({id, start, stop}) => <Flex>{id}: {
-  start?.format(format)} - {
-  stop && stop?.format(format)} = {
-  stop && moment.duration(stop.diff(start)).humanize()} {stop && `(${moment.utc(stop.diff(start))?.format(format)})`}</Flex>
+const colors = {
+  primary: "#c8ffbd",
+  secondary: "#518746",
+  third: "white",
+  fourth: "white"
+}
 
-const ManualInput = ({start: initialStart, stop: initialStop, onAdd, addText = "+"}) => {
+const buttonStyle = {
+  bg: colors.secondary, color: "white"
+}
+
+const padding = 4
+
+const WorkingPeriod = ({id, start, stop}) => <HStack>
+  <Box borderRadius={4} bg={colors.fourth} p={padding}>{start?.format(format)}</Box>
+  <Box>{" - "}</Box>
+  <Box borderRadius={4} bg={colors.fourth} p={padding}>{stop && stop?.format(format)}</Box>
+  <Box>{" = "}</Box>
+  <Box borderRadius={4} bg={colors.fourth} p={padding}>{stop && moment.duration(stop.diff(start)).humanize()} {stop && `(${moment.utc(stop.diff(start))?.format(format)})`}</Box></HStack>
+
+const ManualInput = ({start: initialStart, stop: initialStop, onAdd, addText = "+", isDisabled = false}) => {
   const [start, setStart] = useState(initialStart || "")
   const [stop, setStop] = useState(initialStop || "")
-  return (<Flex maxWidth={300}>
-    <Input placeholder={`start ${formatInput}`} start="start" value={start} onChange={(e) => setStart(e.target.value)}/>
-    <Input placeholder={`stop ${formatInput}`} name="stop" onChange={(e) => setStop(e.target.value)} value={stop} />
-    <Button onClick={() => {
+  return (<HStack bg={colors.primary} padding={padding} spacing={padding} direction="row">
+    <Heading size={"m"}>manual input</Heading>
+    <Input border="white" bg={"white"} width={40} placeholder={`start ${formatInput}`} start="start" value={start} onChange={(e) => setStart(e.target.value)}/>
+    <Input border="white" bg={"white"} width={40} placeholder={`stop ${formatInput}`} name="stop" onChange={(e) => setStop(e.target.value)} value={stop} />
+    <Button disabled={isDisabled} {...buttonStyle} onClick={() => {
       onAdd(start, stop)
       setStart("")
       setStop("")
     }}>{addText}</Button>
-  </Flex>)
+    <Button disabled={isDisabled} {...buttonStyle} onClick={() => {
+      setStart("")
+      setStop("")
+    }}>x</Button>
+  </HStack>)
 }
 
-const WorkingPeriodWithControls = ({start, stop, id, onRemove, onEdit}) => {
+const WorkingPeriodWithControls = ({start, stop, id, onRemove, onEdit, isDisabled = false}) => {
   const [isEdit, setIsEdit] = useState(false)
   return (
-    <Flex>
+    <HStack bg={colors.primary} p={padding}>
       {isEdit
         ? <ManualInput
+          isDisabled={isDisabled}
           addText="save"
           start={start?.format(formatInput)}
           stop={stop?.format(formatInput)}
@@ -43,15 +64,15 @@ const WorkingPeriodWithControls = ({start, stop, id, onRemove, onEdit}) => {
           }}
         />
         : <WorkingPeriod {...{start, stop, id}} />}
-      <Button onClick={() => {
+      <Button disabled={isDisabled} {...buttonStyle} onClick={() => {
         if (!isEdit) {
           setIsEdit(true)
         } else {
           setIsEdit(false)
         }
       }}>{isEdit ? "cancel" : "edit"}</Button>
-      <Button onClick={onRemove}>-</Button>
-    </Flex>
+      <Button disabled={isDisabled} {...buttonStyle} onClick={onRemove}>-</Button>
+    </HStack>
   )
 }
 
@@ -60,7 +81,7 @@ const Summary = ({periods, currentPeriod}) => {
     .filter(p => !!p)
     .map(({start, stop}) => stop.diff(start))
     .reduce((a,b) => a + b, 0)
-  return <Box>total: {moment.utc(moment.duration(durations).asMilliseconds()).format(format)}</Box>
+  return <Box p={padding}>total: {moment.utc(moment.duration(durations).asMilliseconds()).format(format)}</Box>
 }
 
 const removePeriod = (periods, idToRemove) => {
@@ -82,24 +103,30 @@ function App() {
   }, 500)
   return (
     <ChakraProvider>
-      <ManualInput onAdd={(start, stop) => {
+      <ManualInput isDisabled={!!currentPeriod} onAdd={(start, stop) => {
         const startDuartion = moment(start, formatInput)
         const stopDuration = moment(stop, formatInput)
         if (startDuartion.isValid() && stopDuration.isValid()) {
           setWorkingPeriods([...workingPeriods, {start: startDuartion, stop: stopDuration}])
         }
       }} />
-      {!currentPeriod && <Button onClick={() => setCurrentPeriod(moment())}>start</Button>}
-      {currentPeriod && <Button onClick={() => {
+      <Box p={padding}>
+      {!currentPeriod && <Button {...buttonStyle} onClick={() => setCurrentPeriod(moment())}>start</Button>}
+      {currentPeriod && <Button {...buttonStyle} onClick={() => {
         setWorkingPeriods([...workingPeriods, {start: currentPeriod, stop: moment()}])
         setCurrentPeriod(null)
       }}>stop</Button>}
-      current: <WorkingPeriod
-      id={workingPeriods.length}
-      start={currentPeriod || currentMoment}
-      stop={currentPeriod && currentMoment} />
-      <Box>
-        {workingPeriods.filter(({start, stop}) => !!start && !!stop).map((p, i) => <WorkingPeriodWithControls
+        {currentPeriod && <HStack p={padding}>current: <WorkingPeriod
+          id={workingPeriods.length}
+          start={currentPeriod || currentMoment}
+          stop={currentPeriod && currentMoment}/>
+        </HStack>}
+      </Box>
+      <Box p={padding}>
+        {workingPeriods
+          .filter((p) => !!p?.start && !!p?.stop)
+          .map((p, i) => <WorkingPeriodWithControls
+            isDisabled={!!currentPeriod}
           id={i}
           {...p}
           onEdit={(start, stop) => {
@@ -108,7 +135,7 @@ function App() {
           onRemove={() => setWorkingPeriods(removePeriod(workingPeriods, i))}
         />)}
       </Box>
-      <Summary periods={workingPeriods} {...{ currentPeriod }} />
+      <Box bg={colors.third}>{<Summary periods={workingPeriods} {...{currentPeriod}} />}</Box>
     </ChakraProvider>
   );
 }
