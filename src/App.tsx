312ca3946a0abd1517e6b40035aa90e31a0f08bb
Tapio Teras <tapio.teras@reaktor.com>
2020-12-22 11:27:07 +0200

    Refactor the whole app

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,114 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import { ChakraProvider, Input, Heading, Flex, Button, HStack, VStack, Box } from '@chakra-ui/react';
 import {getNearestQuarterMoment} from "./momentHelper";
 import moment from "moment";
 import useInterval from "@use-it/interval";
 
-interface Measurement {
-  start: moment.Moment
-  stop: moment.Moment
+const format = "HH:mm:ss"
+const formatInput = "HH:mm"
+
+const WorkingPeriod = ({id, start, stop}) => <Flex>{id}: {
+  start?.format(format)} - {
+  stop && stop?.format(format)} = {
+  stop && moment.duration(stop.diff(start)).humanize()} {stop && `(${moment.utc(stop.diff(start))?.format(format)})`}</Flex>
+
+const ManualInput = ({start: initialStart, stop: initialStop, onAdd, addText = "+"}) => {
+  const [start, setStart] = useState(initialStart || "")
+  const [stop, setStop] = useState(initialStop || "")
+  return (<Flex maxWidth={300}>
+    <Input placeholder={`start ${formatInput}`} start="start" value={start} onChange={(e) => setStart(e.target.value)}/>
+    <Input placeholder={`stop ${formatInput}`} name="stop" onChange={(e) => setStop(e.target.value)} value={stop} />
+    <Button onClick={() => {
+      onAdd(start, stop)
+      setStart("")
+      setStop("")
+    }}>{addText}</Button>
+  </Flex>)
+}
+
+const WorkingPeriodWithControls = ({start, stop, id, onRemove, onEdit}) => {
+  const [isEdit, setIsEdit] = useState(false)
+  return (
+    <Flex>
+      {isEdit
+        ? <ManualInput
+          addText="save"
+          start={start?.format(formatInput)}
+          stop={stop?.format(formatInput)}
+          onAdd={(newStart, newStop) => {
+            const newStartDuration = moment(newStart, formatInput)
+            const newStopDuration = moment(newStop, formatInput)
+            onEdit(newStartDuration, newStopDuration)
+            setIsEdit(false)
+          }}
+        />
+        : <WorkingPeriod {...{start, stop, id}} />}
+      <Button onClick={() => {
+        if (!isEdit) {
+          setIsEdit(true)
+        } else {
+          setIsEdit(false)
+        }
+      }}>{isEdit ? "cancel" : "edit"}</Button>
+      <Button onClick={onRemove}>-</Button>
+    </Flex>
+  )
+}
+
+const Summary = ({periods, currentPeriod}) => {
+  const durations = [...periods, currentPeriod ? {start: currentPeriod, stop: moment()} : null]
+    .filter(p => !!p)
+    .map(({start, stop}) => stop.diff(start))
+    .reduce((a,b) => a + b, 0)
+  return <Box>total: {moment.utc(moment.duration(durations).asMilliseconds()).format(format)}</Box>
+}
+
+const removePeriod = (periods, idToRemove) => {
+  let newPeriods = periods
+  delete newPeriods?.[idToRemove]
+  return newPeriods
+}
+
+const editPeriod = (periods, idToEdit, newStart, newStop) => {
+  return [...periods].map((p, i) => i === idToEdit ? {start: newStart, stop: newStop} : p)
 }
 
 function App() {
-  const [startMoment, setStartMoment] = useState()
+  const [workingPeriods, setWorkingPeriods] = useState([])
+  const [currentPeriod, setCurrentPeriod] = useState(null)
   const [currentMoment, setCurrentMoment] = useState(moment())
-  const [measurements, setMeasurements] = useState([])
   useInterval(() => {
-      setCurrentMoment(moment())
-  }, 100);
-  const diff = (start: moment.Moment, stop: moment.Moment) => moment.utc(stop.diff(start))
-  const toMeasurementRow = (start: moment.Moment, stop: moment.Moment) => <>
-    {start.format("HH:mm")} - {stop.format("HH:mm")} = {
-    diff(start, stop).format("HH:mm")}
-    ({getNearestQuarterMoment(start).format("HH:mm")} - {getNearestQuarterMoment(stop).format("HH:mm")} = {getNearestQuarterMoment(diff(start, stop)).format("HH:mm")})
-  </>
-  const measurementsAsMilliseconds = () => [...measurements]
-    .map(({start, stop}) => diff(start, stop))
-    .map(duration => duration.milliseconds())
-  const onStart = () => {
-    setStartMoment(moment())
-  }
-  const onStop = () => {
-    setMeasurements([...measurements, { start: startMoment, stop: moment() } as Measurement])
-    setStartMoment(undefined)
-  }
-  const totalTime = moment.utc(measurementsAsMilliseconds().reduce((a,b) => a + b, 0)).format("HH:mm")
+    setCurrentMoment(moment())
+  }, 500)
   return (
     <ChakraProvider>
-      <Heading size="1xl">{currentMoment.format("HH:mm")}</Heading>
-      <Button disabled={!!startMoment} onClick={onStart}>start</Button>
-      {startMoment && <>
-          <Button onClick={onStop}>stop</Button>
-          <VStack>
-        {startMoment && <Box backgroundColor="green" spacing={10}>{toMeasurementRow(startMoment, moment())}</Box>}
-      </VStack>
-      </>}
-      <VStack>{measurements.reverse().map(({start, stop}, i) =>
-        <Box spacing={5} key={`measurement-row-${i}`}>
-          {toMeasurementRow(start, stop)}
-        </Box>)}</VStack>
-      <Box>total: {totalTime}</Box>
+      <ManualInput onAdd={(start, stop) => {
+        const startDuartion = moment(start, formatInput)
+        const stopDuration = moment(stop, formatInput)
+        if (startDuartion.isValid() && stopDuration.isValid()) {
+          setWorkingPeriods([...workingPeriods, {start: startDuartion, stop: stopDuration}])
+        }
+      }} />
+      {!currentPeriod && <Button onClick={() => setCurrentPeriod(moment())}>start</Button>}
+      {currentPeriod && <Button onClick={() => {
+        setWorkingPeriods([...workingPeriods, {start: currentPeriod, stop: moment()}])
+        setCurrentPeriod(null)
+      }}>stop</Button>}
+      current: <WorkingPeriod
+      id={workingPeriods.length}
+      start={currentPeriod || currentMoment}
+      stop={currentPeriod && currentMoment} />
+      <Box>
+        {workingPeriods.filter(({start, stop}) => !!start && !!stop).map((p, i) => <WorkingPeriodWithControls
+          id={i}
+          {...p}
+          onEdit={(start, stop) => {
+            setWorkingPeriods([...editPeriod(workingPeriods, i, start, stop)])
+          }}
+          onRemove={() => setWorkingPeriods(removePeriod(workingPeriods, i))}
+        />)}
+      </Box>
+      <Summary periods={workingPeriods} {...{ currentPeriod }} />
     </ChakraProvider>
   );
 }
